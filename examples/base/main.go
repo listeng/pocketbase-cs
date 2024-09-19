f548fd3ec3ba81e081513c81a32d43b0c6070a06
tls <aosp@163.com>
2024-09-19 19:33:07 +0800

    Add BaseURL to static url prefix

--- examples/base/main.go
+++ examples/base/main.go
@@ -117,7 +117,7 @@ func main() {
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		// serves static files from the provided public dir (if exists)
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS(publicDir), indexFallback))
+		e.Router.GET(apis.BaseUrl+"/*", apis.StaticDirectoryHandler(os.DirFS(publicDir), indexFallback))
 		return nil
 	})
 
